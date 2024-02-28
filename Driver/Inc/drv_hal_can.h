bb80291c4d132527b579c483917b0de17cabed42
PoisonNF <903131263@qq.com>
2024-02-28 14:38:44 +0800

    feat: CAN增加中断相关的函数

--- Driver/Inc/drv_hal_can.h
+++ Driver/Inc/drv_hal_can.h
@@ -5,18 +5,53 @@
 
 typedef struct
 {
-    CAN_HandleTypeDef tCANHandle;           /* CAN句柄 */
-    CAN_TxHeaderTypeDef tCANTxHeader;       /* CAN发送句柄 */
-    CAN_RxHeaderTypeDef tCANRxHeader;       /* CAN接收句柄 */
-    CAN_FilterTypeDef tCANFilter;           /* CAN过滤器配置句柄 */
-    tagGPIO_T tGPIO[2];                     /* GPIO配置 */
+    CAN_HandleTypeDef   tCANHandle;             /* CAN句柄 */
+    CAN_TxHeaderTypeDef tCANTxHeader;           /* CAN发送句柄 */
+    CAN_RxHeaderTypeDef tCANRxHeader;           /* CAN接收句柄 */
+    CAN_FilterTypeDef   tCANFilter;             /* CAN过滤器配置句柄 */
+    tagGPIO_T           tGPIO[2];               /* GPIO配置 */
 
+    uint8_t				ucTxPriority;		    /* 发送中断优先级，0-15 */
+	uint8_t 			ucTxSubPriority;	    /* 发送中断子优先级，0-15 */
+
+    uint8_t				ucRxPriority;		    /* 接收中断优先级，0-15 */
+	uint8_t 			ucRxSubPriority;	    /* 接收中断子优先级，0-15 */
 }tagCAN_T;
+
+/* 中断回调函数 */
+/*
+    //CAN通信-发送完成回调函数
+    void HAL_CAN_TxMailbox0CompleteCallback(CAN_HandleTypeDef *hcan);
+    void HAL_CAN_TxMailbox1CompleteCallback(CAN_HandleTypeDef *hcan);
+    void HAL_CAN_TxMailbox2CompleteCallback(CAN_HandleTypeDef *hcan);
+    //CAN通信-发送取消回调函数
+    void HAL_CAN_TxMailbox0AbortCallback(CAN_HandleTypeDef *hcan);
+    void HAL_CAN_TxMailbox1AbortCallback(CAN_HandleTypeDef *hcan);
+    void HAL_CAN_TxMailbox2AbortCallback(CAN_HandleTypeDef *hcan);
+
+    //CAN通信-FIFO0接收新消息回调函数
+    void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan);
+    //CAN通信-FIFO0接收满回调函数
+    void HAL_CAN_RxFifo0FullCallback(CAN_HandleTypeDef *hcan);
+    //CAN通信-FIFO1接收新消息回调函数
+    void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan);
+    //CAN通信-FIFO1接收满回调函数
+    void HAL_CAN_RxFifo1FullCallback(CAN_HandleTypeDef *hcan);
+
+    //CAN通信-休眠回调函数
+    void HAL_CAN_SleepCallback(CAN_HandleTypeDef *hcan);
+    //CAN通信-唤醒回调函数
+    void HAL_CAN_WakeUpFromRxMsgCallback(CAN_HandleTypeDef *hcan);
+    //CAN通信-错误回调函数
+    void HAL_CAN_ErrorCallback(CAN_HandleTypeDef *hcan);
+*/
 
 void Drv_CAN_ModeConfig(tagCAN_T *_tCAN,uint32_t _ulMode);
 void Drv_CAN_TxIDConfig(tagCAN_T *_tCAN,uint32_t _ulID);
 uint8_t Drv_CAN_SendMsg(tagCAN_T *_tCAN,uint8_t *_ucpMsg,uint8_t _ucLen);
 uint8_t Drv_CAN_ReceMsg(tagCAN_T *_tCAN,uint8_t *_ucpMsg);
 void Drv_CAN_Init(tagCAN_T *_tCAN);
+void Drv_CAN_IRQHandler(tagCAN_T *_tCAN);
 
 #endif
+
--- Driver/Src/drv_hal_can.c
+++ Driver/Src/drv_hal_can.c
@@ -9,6 +9,8 @@
 * 文件历史：
 
 * 版本号	日期		作者		说明
+*    	2023-02-28	  鲍程璐	增加CAN中断相关函数
+
 *  3.0 	2023-01-26	  鲍程璐	适配STM32F4系列
 
         2024-01-02	  鲍程璐	修改形参符合规范
@@ -80,6 +82,55 @@ static void S_CAN_GPIOConfig(tagCAN_T *_tCAN)
 }
 
 /**
+ * @brief CAN中断配置函数
+ * @param _tCAN-CAN结构体指针
+ * @retval Null 
+*/
+static void S_CAN_NVICConfig(tagCAN_T *_tCAN)
+{
+    if(_tCAN->tCANHandle.Instance == CAN1)
+    {
+        /* CAN1发送中断配置 */
+        HAL_NVIC_SetPriority(CAN1_TX_IRQn,_tCAN->ucTxPriority,_tCAN->ucTxSubPriority);
+        HAL_NVIC_EnableIRQ(CAN1_TX_IRQn);
+
+        /* CAN1接收0中断配置 */
+        HAL_NVIC_SetPriority(CAN1_RX0_IRQn,_tCAN->ucTxPriority,_tCAN->ucTxSubPriority);
+        HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
+
+        /* CAN1接收1中断配置 */
+        HAL_NVIC_SetPriority(CAN1_RX1_IRQn,_tCAN->ucTxPriority,_tCAN->ucTxSubPriority);
+        HAL_NVIC_EnableIRQ(CAN1_RX1_IRQn);
+
+        /* CAN1总线状态改变中断配置 */
+        HAL_NVIC_SetPriority(CAN1_SCE_IRQn,0,0);
+        HAL_NVIC_EnableIRQ(CAN1_SCE_IRQn);
+    }
+
+#ifdef STM32F4_SGA_ENABLE
+    else if(_tCAN->tCANHandle.Instance == CAN2)
+    {
+        /* CAN2发送中断配置 */
+        HAL_NVIC_SetPriority(CAN2_TX_IRQn,_tCAN->ucTxPriority,_tCAN->ucTxSubPriority);
+        HAL_NVIC_EnableIRQ(CAN2_TX_IRQn);
+
+        /* CAN2接收0中断配置 */
+        HAL_NVIC_SetPriority(CAN2_RX0_IRQn,_tCAN->ucTxPriority,_tCAN->ucTxSubPriority);
+        HAL_NVIC_EnableIRQ(CAN2_RX0_IRQn);
+
+        /* CAN2接收1中断配置 */
+        HAL_NVIC_SetPriority(CAN2_RX1_IRQn,_tCAN->ucTxPriority,_tCAN->ucTxSubPriority);
+        HAL_NVIC_EnableIRQ(CAN2_RX1_IRQn);
+
+        /* CAN2总线状态改变中断配置 */
+        HAL_NVIC_SetPriority(CAN2_SCE_IRQn,0,0);
+        HAL_NVIC_EnableIRQ(CAN2_SCE_IRQn);
+    }
+#endif
+
+}
+
+/**
  * @brief CAN参数设置
  * @param _tCAN-CAN结构体指针
  * @retval Null
@@ -98,8 +149,26 @@ static void S_CAN_ParamConfig(tagCAN_T *_tCAN)
     if(HAL_CAN_Start(&_tCAN->tCANHandle) != HAL_OK)
         Drv_HAL_Error(__FILE__,__LINE__);
 
-    /* 激活RX通知 */
-    if(HAL_CAN_ActivateNotification(&_tCAN->tCANHandle, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+    /* 激活CAN发送中断通知 */
+    if(HAL_CAN_ActivateNotification(&_tCAN->tCANHandle,CAN_IT_TX_MAILBOX_EMPTY) != HAL_OK)
+        Drv_HAL_Error(__FILE__,__LINE__);
+
+    /* 根据FIFO选择 */
+    if(_tCAN->tCANFilter.FilterFIFOAssignment == CAN_RX_FIFO0)
+    {
+        /* 激活FIFO0接收新消息通知 */
+        if(HAL_CAN_ActivateNotification(&_tCAN->tCANHandle, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+            Drv_HAL_Error(__FILE__,__LINE__);
+    }
+    else
+    {
+        /* 激活FIFO1接收新消息通知 */
+        if(HAL_CAN_ActivateNotification(&_tCAN->tCANHandle, CAN_IT_RX_FIFO1_MSG_PENDING) != HAL_OK)
+            Drv_HAL_Error(__FILE__,__LINE__);
+    }
+
+    /* 激活CAN错误中断通知 */
+    if(HAL_CAN_ActivateNotification(&_tCAN->tCANHandle, CAN_IT_ERROR) != HAL_OK)
         Drv_HAL_Error(__FILE__,__LINE__);
 }
 
@@ -152,7 +221,7 @@ uint8_t Drv_CAN_SendMsg(tagCAN_T *_tCAN,uint8_t *ucpMsg,uint8_t ucLen)
 
     _tCAN->tCANTxHeader.DLC = ucLen;    /* 发送长度设置 */
     
-    ulTXMailBox = CAN_TX_MAILBOX1;      /* 发送邮箱设置 */
+    ulTXMailBox = CAN_TX_MAILBOX0;      /* 发送邮箱设置 */
 
     /* 数据拷贝 */
     for(index = 0;index < ucLen;index ++)
@@ -202,7 +271,19 @@ void Drv_CAN_Init(tagCAN_T *_tCAN)
 {
 	S_CAN_CLKEnable(_tCAN);
 	S_CAN_GPIOConfig(_tCAN);
+    S_CAN_NVICConfig(_tCAN);
 	S_CAN_ParamConfig(_tCAN);
+}
+
+/**
+ * @brief CAN中断帽子函数(用于CAN中断处理函数中)
+ * @note 在类似void CAN1_TX_IRQHandler(void)函数中调用，根据实际情况进行更改
+ * @param _tCAN-CAN结构体指针
+ * @retval Null
+*/
+void Drv_CAN_IRQHandler(tagCAN_T *_tCAN)
+{
+	HAL_CAN_IRQHandler(&_tCAN->tCANHandle);
 }
 
 #endif
